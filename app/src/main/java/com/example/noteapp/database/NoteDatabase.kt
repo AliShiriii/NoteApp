ac124f19a82e601ffee3b4fa3a318cba796d881e
Ali__Shiri <alishiri067@gmail.com>
2021-12-23 13:49:48 -0800

    edit dataBase in project

--- app/src/main/java/com/example/noteapp/database/NoteDatabase.kt
+++ app/src/main/java/com/example/noteapp/database/NoteDatabase.kt
@@ -8,6 +8,7 @@ import com.example.noteapp.model.Note
 @Database(entities = [Note::class], version = 4, exportSchema = false)
 abstract class NoteDatabase : RoomDatabase() {
 
+    //fun getDao from dao
     abstract fun getNote(): NoteDao
 
 }
