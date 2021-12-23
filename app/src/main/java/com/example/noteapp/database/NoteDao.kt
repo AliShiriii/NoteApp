5aeb73f5bc031515735d9d44b29bc8f02053f378
Ali__Shiri <alishiri067@gmail.com>
2021-12-23 13:44:03 -0800

    add Dao in project

--- app/src/main/java/com/example/noteapp/database/NoteDao.kt
+++ app/src/main/java/com/example/noteapp/database/NoteDao.kt
@@ -7,18 +7,23 @@ import com.example.noteapp.model.Note
 @Dao
 interface NoteDao {
 
+    //Insert
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNote(note: Note)
 
+    //Update
     @Update()
     suspend fun updateNote(note: Note)
 
+    //Delete
     @Delete()
     suspend fun deleteNote(note: Note)
 
+    //Query for get all notes
     @Query("SELECT * FROM notes ORDER BY id DESC")
     fun getAlNotes(): LiveData<List<Note>>
 
+    //Query fore search notes
     @Query("SELECT * FROM notes ORDER BY notTitle LIKE :query OR noteBode LIKE :query")
     fun searchNotes(query: String?): LiveData<List<Note>>
 }
