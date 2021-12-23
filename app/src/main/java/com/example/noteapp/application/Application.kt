e114409ae8e66c71c944abb4f47f5faaf40d8a94
Ali__Shiri <alishiri067@gmail.com>
2021-12-23 13:13:51 -0800

    edit baseApplication

--- app/src/main/java/com/example/noteapp/application/Application.kt
+++ app/src/main/java/com/example/noteapp/application/Application.kt
@@ -1,7 +1,17 @@
 package com.example.noteapp.application
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class Application: Application()
+class Application: Application(){
+
+    //BaseApplication
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+
+    }
+}
