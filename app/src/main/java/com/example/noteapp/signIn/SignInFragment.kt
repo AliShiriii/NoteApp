ab6aee069677b23392673d5a5ea0a08fabd578eb
AliShiri <Your new email>
2022-12-08 03:01:57 -0800

    add SignInFragment

---
+++ app/src/main/java/com/example/noteapp/signIn/SignInFragment.kt
@@ -0,0 +1,32 @@
+package com.example.noteapp.signIn
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.noteapp.databinding.FragmentSignInBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignInFragment : Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
