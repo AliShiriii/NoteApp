4b83f82e0cc9c0a64a68b4dbe1a6d0c691725185
AliShiri <Your new email>
2022-12-16 03:41:21 -0800

    add Binding in LoginFragment

--- app/src/main/java/com/example/noteapp/login/LoginFragment.kt
+++ app/src/main/java/com/example/noteapp/login/LoginFragment.kt
@@ -5,29 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.noteapp.R
 import com.example.noteapp.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginFragment : Fragment(R.layout.fragment_login) {
+class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        // Inflate the layout for this fragment
 
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         return binding.root
     }
+
+
+
 }
