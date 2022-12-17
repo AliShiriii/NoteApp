c542ebd0a4f2631cfbd3be10763e9eec12ae83c0
AliShiri <Your new email>
2022-12-17 03:37:27 -0800

    compeleted loginFragment

--- app/src/main/java/com/example/noteapp/login/LoginFragment.kt
+++ app/src/main/java/com/example/noteapp/login/LoginFragment.kt
@@ -3,9 +3,14 @@ package com.example.noteapp.login
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.noteapp.databinding.FragmentLoginBinding
+import com.example.noteapp.utils.Contracts.PASSWORD_ID
+import com.example.noteapp.utils.Contracts.USER_ID
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,6 +18,8 @@ class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +32,41 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        login()
+
+    }
+
+
+    //login
+    private fun login() {
+
+        binding.loginButton.setOnClickListener {
+
+            if (binding.email.text.toString() == USER_ID
+                && binding.password.text.toString() == PASSWORD_ID
+            ) {
+
+               viewModel.loginUser(USER_ID, PASSWORD_ID)
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSplashFragment())
+
+            } else if (binding.email.text.toString() != USER_ID
+                && binding.password.text.toString() != PASSWORD_ID
+            ) {
+
+                binding.checkEmailAddress.visibility = VISIBLE
+
+            } else if (binding.email.text.toString().isNullOrEmpty()
+                && binding.password.text.toString().isNullOrEmpty()
+            ) {
+
+                binding.checkEmailAddress.text = "Please enter your email and password!"
+                binding.checkEmailAddress.visibility = VISIBLE
+
+            }
+        }
+    }
 
 }
